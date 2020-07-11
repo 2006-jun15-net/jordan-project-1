9c4dca7aa4167c5ba66e4e1a0db046aa86a60b3c
Jordan Gamble <jgamble482@gmail.com>
2020-07-11 19:44:27 -0400

    Started MVC: Added Customer and Order ViewModels

---
+++ HardWaxReborn/HardWaxReborn/Models/CustomerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using HardWaxReborn.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardWaxReborn.Models
+{
+    public class CustomerViewModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        public List<Order> OrderHistory { get; set; }
+    }
+}
---
+++ HardWaxReborn/HardWaxReborn/Models/OrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using HardWaxReborn.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardWaxReborn.Models
+{
+    public class OrderViewModel
+    {
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+
+        public DateTime OrderTime { get; set; }
+    }
+}
